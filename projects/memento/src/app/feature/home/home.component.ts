41693dad013fb193f13b46aad2693815dfadc645
Nicolas Rannou <nicolas.rannou@spinergie.com>
2024-07-18 12:18:10 +0200

    test

--- projects/memento/src/app/feature/home/home.component.ts
+++ projects/memento/src/app/feature/home/home.component.ts
@@ -1,11 +1,9 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
 @Component({
   selector: 'hostiles-home',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>Welcome to digest App</p>`,
+  imports: [],
+  template: ``,
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
--- projects/memento/src/app/feature/home/home.routes.ts
+++ projects/memento/src/app/feature/home/home.routes.ts
@@ -1,4 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { HomeAComponent } from './home-a/home-a.component';
+import { HomeBComponent } from './home-b/home-b.component';
 
-export default <Routes>[{ path: '', component: HomeComponent }];
+export default <Routes>[
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: 'home-a', component: HomeAComponent },
+      { path: 'home-b', component: HomeBComponent },
+    ],
+  },
+];
