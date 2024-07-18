374ebde38d3ac63f272f52190e622af07a706192
Nicolas Rannou <nicolas.rannou@spinergie.com>
2024-07-18 12:21:24 +0200

    This is my final comment
    * test
    
    * test
    
    * test2

---
+++ projects/memento/src/app/feature/home/home-a/home-a.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'hostiles-home-a',
+  template: ` <h1>Home A</h1> `,
+})
+export class HomeAComponent {}
---
+++ projects/memento/src/app/feature/home/home-b/home-b.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'hostiles-home-b',
+  template: ` <h1>Home B</h1> `,
+})
+export class HomeBComponent {}
